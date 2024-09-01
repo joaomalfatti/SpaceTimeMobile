bfd6a32f10ba12aee9356f49240e5d4e40e35fb5
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-01 11:46:32 -0300

    Foi adicionado cores globais e também as fontes do projeto

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
+/* Importando a fonte roboto */
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+/* Importando a fonte bai-jamjuree */
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree';
 
 export default function App() {
+  /*Constante para trazer as fontes*/
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  /*Uma verificação para trazer as fontes, caso não trazer, returna null*/
+  if (!hasLoadedFonts){
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex items-center justify-center bg-gray-900">
+      <Text className="text-5xl font-bold text-gray-50"></Text>
+      <StatusBar style="light" translucent />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
