7f79e24d2bbce73298d5c8e90efce72d103f9ef5
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-01 20:43:49 -0300

    Foi finalizado homepage do mobile, feito as configurações necessárias e estruturação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {ImageBackground } from 'react-native';
+import {ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 
 /* Importando a fonte roboto */
 import {
@@ -16,6 +16,14 @@ import {
 /* Importando a imagem do blur */
 import blurBg from './src/assets/bg-blur.png';
 
+/* Importando a imagem do Stripes e preciso colocar S maiúsculo por causa que ele vai ser um componente*/
+import Stripes from './src/assets/stripes.svg';
+import {styled} from 'nativewind'
+const StyledStripes = styled(Stripes);
+
+/* Importando a logo*/
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+
 export default function App() {
   /*Constante para trazer as fontes*/
   const [hasLoadedFonts] = useFonts({
@@ -31,9 +39,40 @@ export default function App() {
   return (
     <ImageBackground 
       source={blurBg} 
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{position: "absolute", left: '-100%'}}
       >
+        <StyledStripes className="absolute left-2"/>
+
+        <View className="flex-1 items-center justify-center gap-6">
+          <NLWLogo />
+          {/* Aqui seria parte dos texto*/}
+          <View>
+            <Text className="text-center font-title text-2xl leading-tight text-gray-50"> Sua cápsula do tempo</Text>
+            <Text className="text-center font-body text-base leading-relaxed text-gray-100"> Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+              com o mundo!
+            </Text>
+          </View>
+
+          {/* Aqui seria parte do botão*/}
+          <TouchableOpacity
+            activeOpacity={0.7}
+            className="rounded-full bg-green-500 px-5 py-2"
+          >
+            <Text className="font-alt text-sm uppercase text-black">
+              Cadastrar lembrança
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+
+        {/* Footer do aplicativo*/}
+        <Text 
+          className=" text-center font-body text-sm leading-relaxed text-gray-200"
+        > 
+          Feito com 💛 no NLW João Elias
+        </Text>
+        
         <StatusBar style="light" translucent />
     </ImageBackground>
   );
